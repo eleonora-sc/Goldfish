6660548c0d94e8bdb19c42aa6e433ddd5030177d
jaren-ramirez <jarenramirez2019@gmail.com>
2023-10-15 11:57:39 -0800

    hello eleonora

---
+++ create_measurements/create_measurements.py
@@ -0,0 +1,96 @@
+from ripe.atlas.cousteau import AtlasSource, AtlasCreateRequest, Ping, Traceroute, Probe
+from ripe.atlas.sagan import SslResult
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import pandas as pd
+
+class CreateMeasurements:
+    def __init__(self, ATLAS_API_KEY:str):
+        self.ATLAS_API_KEY = ATLAS_API_KEY
+        self.targets = None
+        self.target_ips = None
+        self.sources = None
+        self.sources_str = ""
+        self.return_data = None
+        self.logger = None
+
+    def load_logger(self, logger):
+        self.logger = logger
+    
+    def __load_target_ips(self):
+        target_ips = []
+        for i, target in enumerate(self.targets):
+            probe = Probe(id=target)
+            target_ips.append(probe.address_v4)
+            if self.logger:
+                self.logger.log(f"{probe.country_code} - {probe.address_v4}")
+        self.target_ips = target_ips
+        print(self.target_ips)
+
+    def load_targets(self, targets:dict[int, str]):
+        """
+        This sets self.targets to be used in creating measurements and sets the ips associated with them
+        Args:
+            :targets dictionary of the target probes in the format {probe_id:"City Name"}
+            :type dict[int, str]
+        """
+        self.targets = targets
+        self.__load_target_ips()
+
+    def load_sources(self, source:dict[int,str]):
+        self.sources = source
+        for i,key in enumerate(self.sources):
+            if i < len(self.sources)-1:
+                self.sources_str  = self.sources_str + str(key)+","
+            else:
+                self.sources_str = self.sources_str + str(key)
+        print(self.sources_str)
+
+
+    def create_ping_measurements(self):
+        pass
+
+    def create_traceroute_measurements(self):
+        measurement_data = pd.DataFrame({
+            "source":[],
+            "target":[],
+            "measurement_id":[]
+
+        })
+        if(self.target_ips == None or self.sources == None or self.sources_str == "" or self.targets == None):
+            raise ValueError("All data not loaded in")
+        for i, target in enumerate(self.target_ips):
+            print(f"trying target {i}, with target {target}")
+            traceroute = Traceroute(
+                af=4, # address family ie ipv4 and ipv6
+                target = target,
+                description = f"Goldfish OFFICIAL traceroute measurement to {target}",
+                protocol="ICMP",
+            )
+            source = AtlasSource(
+                type="probes",
+                value=self.sources_str, # must be in this format
+                requested=1,
+                tags={"include":["system-ipv4-works"]},
+            )
+            atlas_request = AtlasCreateRequest(
+                start_time=datetime.utcnow(),
+                key=self.ATLAS_API_KEY,
+                measurements=[traceroute],
+                sources=[source],
+                is_oneoff=True
+            )
+
+            (is_success, response) = atlas_request.create()
+            print(is_success)
+            print(response)
+            temp_dataframe = pd.DataFrame({
+            "source":[self.sources_str],
+            "target":[target],
+            "measurement_id":[int(response['measurements'][0])]
+
+            })
+            measurement_data = pd.concat([measurement_data,temp_dataframe],ignore_index=True)
+        measurement_data.to_csv("./measurements.csv",mode='a')
+
---
+++ main.py
@@ -0,0 +1,15 @@
+from logger import Logger
+from create_measurements.create_measurements import CreateMeasurements
+from dotenv import load_dotenv
+from os import getenv
+if __name__ == "__main__":
+    load_dotenv()
+    logger = Logger()
+    ATLAS_API_KEY = getenv("ATLAS_API_KEY")
+    create_measurements = CreateMeasurements(ATLAS_API_KEY)
+    alaska_nodes = {14300: "Juneau", 51310: "Anchorage 2", 51754: "Anchorage 3", 52344: "Eagle River", 60259: "Anchorage 4", 61113: "Palmer", 61868: "Anchorage 1"}
+    # list of targets for traceroute measurements in key value pairs
+    traceroute_targets = {62796: "Lagos", 1004023: "Rio", 1005456: "New Delhi", 1006001: "Berlin", 1002558: "Sydney"}
+    create_measurements.load_sources(alaska_nodes)
+    create_measurements.load_targets(traceroute_targets)
+    create_measurements.create_traceroute_measurements()
